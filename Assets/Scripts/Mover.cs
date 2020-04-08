403ee6f096e27cc10b0cc0881596932184bd5609
Salikh Magomedov <sm.saarus@gmail.com>
2020-04-08 15:12:06 +0300

    Introduction To Raycasting

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,15 +4,25 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] private Transform target;
+
     private NavMeshAgent _agent;
+    private Ray _lastRay;
+    private Camera _camera;
 
     private void Awake()
     {
+        _camera = Camera.main;
         _agent = GetComponent<NavMeshAgent>();
     }
 
     private void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _lastRay = _camera.ScreenPointToRay(Input.mousePosition);
+        }
+
+        Debug.DrawRay(_lastRay.origin, _lastRay.direction * 100);
         _agent.destination = target.position;
     }
 }
