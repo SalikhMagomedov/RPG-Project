e5f59f37fbd5d83a5d9a300797989d0283074649
Salikh Magomedov <sm.saarus@gmail.com>
2020-04-10 22:53:41 +0300

    Namespaces To See Dependencies

---
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Core
+{
+    public class FollowCamera : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+
+        private void LateUpdate()
+        {
+            transform.position = target.position;
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Movement
+{
+    public class Mover : MonoBehaviour
+    {
+        private NavMeshAgent _agent;
+        private Animator _animator;
+
+        private static readonly int Property = Animator.StringToHash("Forward Speed");
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            // if (Input.GetMouseButton(0)) MoveToCursor();
+            UpdateAnimator();
+        }
+
+        private void UpdateAnimator()
+        {
+            var velocity = _agent.velocity;
+            var localVelocity = transform.InverseTransformDirection(velocity);
+            var speed = localVelocity.z;
+
+            _animator.SetFloat(Property, speed);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            _agent.destination = destination;
+        }
+    }
+}
