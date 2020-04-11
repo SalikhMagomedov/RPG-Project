7ebf302707439723d9e399a63d428a464a2f5c7c
Salikh Magomedov <sm.saarus@gmail.com>
2020-04-11 20:58:39 +0300

    Trigger Death Animation

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -1,15 +1,26 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Combat
 {
     public class Health : MonoBehaviour
     {
-         [SerializeField] private float health = 100f;
+        [SerializeField] private float health = 100f;
+        private static readonly int DieTrigger = Animator.StringToHash("Die");
 
-         public void TakeDamage(float damage)
-         {
-             health = Mathf.Max(health - damage, 0);
-             print(health);
-         }
+        private bool _isDead;
+
+        public void TakeDamage(float damage)
+        {
+            health = Mathf.Max(health - damage, 0);
+            if (Math.Abs(health) < float.Epsilon) Die();
+        }
+
+        private void Die()
+        {
+            if (_isDead) return;
+            GetComponent<Animator>().SetTrigger(DieTrigger);
+            _isDead = true;
+        }
     }
 }
