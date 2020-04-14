435a624dc7bf3f304c7b26d1c8286a67e6653fee
Salikh Magomedov <sm.saarus@gmail.com>
2020-04-14 16:11:23 +0300

    That's Some Suspicious Behaviour

--- Assets/Scripts/Control/AiController.cs
+++ Assets/Scripts/Control/AiController.cs
@@ -7,16 +7,20 @@ namespace Control
 {
     public class AiController : MonoBehaviour
     {
+        private ActionScheduler _actionScheduler;
         private Fighter _fighter;
-        private Health _health;
-        private GameObject _player;
-        private Mover _mover;
         private Vector3 _guardPosition;
+        private Health _health;
+        private Mover _mover;
+        private GameObject _player;
+        private float _timeSinceLastSawPlayer = Mathf.Infinity;
 
         [SerializeField] private float chaseDistance = 5f;
+        [SerializeField] private float suspicionTime = 5f;
 
         private void Awake()
         {
+            _actionScheduler = GetComponent<ActionScheduler>();
             _mover = GetComponent<Mover>();
             _health = GetComponent<Health>();
             _fighter = GetComponent<Fighter>();
@@ -32,9 +36,35 @@ namespace Control
         {
             if (_health.IsDead) return;
             if (InAttackRangeOfPlayer() && _fighter.CanAttack(_player))
-                _fighter.Attack(_player);
+            {
+                _timeSinceLastSawPlayer = 0;
+                AttackBehaviour();
+            }
+            else if (_timeSinceLastSawPlayer < suspicionTime)
+            {
+                SuspicionBehaviour();
+            }
             else
-                _mover.StartMoveAction(_guardPosition);
+            {
+                GuardBehaviour();
+            }
+
+            _timeSinceLastSawPlayer += Time.deltaTime;
+        }
+
+        private void GuardBehaviour()
+        {
+            _mover.StartMoveAction(_guardPosition);
+        }
+
+        private void SuspicionBehaviour()
+        {
+            _actionScheduler.CancelCurrentAction();
+        }
+
+        private void AttackBehaviour()
+        {
+            _fighter.Attack(_player);
         }
 
         private bool InAttackRangeOfPlayer()
