2dc3ca15091ce356726ea5c7486222b4e522d523
Salikh Magomedov <sm.saarus@gmail.com>
2020-04-14 16:22:58 +0300

    Visualising Waypoints

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        private const float WaypointGizmoRadius = .3f;
+
+        private void OnDrawGizmos()
+        {
+            for (var i = 0; i < transform.childCount; i++)
+            {
+                Gizmos.DrawSphere(transform.GetChild(i).position, WaypointGizmoRadius);
+            }
+        }
+    }
+}
