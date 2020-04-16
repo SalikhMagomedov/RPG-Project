e634ca97bc760a38e2754c4530a146ec8012cb89
Salikh Magomedov <sm.saarus@gmail.com>
2020-04-16 14:34:46 +0300

    Trigger Cinemachine Cut Scene

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+namespace Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        private bool _alreadyTriggered;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Player") && !_alreadyTriggered) return;
+
+            GetComponent<PlayableDirector>().Play();
+            _alreadyTriggered = true;
+        }
+    }
+}
