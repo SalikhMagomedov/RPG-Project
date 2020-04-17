7b26e4bf633df3cdb116d17d554803df3610a367
Salikh Magomedov <sm.saarus@gmail.com>
2020-04-17 12:59:44 +0300

    Scene Loading Portals

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] private int sceneToLoad = -1;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Player")) return;
+
+            SceneManager.LoadSceneAsync(sceneToLoad);
+        }
+    }
+}
