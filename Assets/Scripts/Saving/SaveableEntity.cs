972bfc7fa94d91d88cd76225b6b6827db6895e1c
Salikh Magomedov <sm.saarus@gmail.com>
2020-04-20 15:12:49 +0300

    Editing SerializedFields

--- Assets/Scripts/Saving/SaveableEntity.cs
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEditor;
+using UnityEngine;
 
 namespace RPG.Saving
 {
@@ -15,11 +17,19 @@ namespace RPG.Saving
         {
         }
 
+#if UNITY_EDITOR
         private void Update()
         {
-            if (Application.isPlaying) return;
+            if (Application.isPlaying || string.IsNullOrEmpty(gameObject.scene.path)) return;
+
+            var serializedObject = new SerializedObject(this);
+            var property = serializedObject.FindProperty("uniqueIdentifier");
+
+            if (!string.IsNullOrEmpty(property.stringValue)) return;
             
-            print("Editing;");
+            property.stringValue = Guid.NewGuid().ToString();
+            serializedObject.ApplyModifiedProperties();
         }
+#endif
     }
 }
