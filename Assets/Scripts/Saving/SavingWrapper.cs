3afbf4411a54638e00aaeba9295042d9d7fbe9b3
Salikh Magomedov <sm.saarus@gmail.com>
2020-04-20 13:05:46 +0300

    Save System Design

---
+++ Assets/Scripts/Saving/SavingWrapper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace RPG.Saving
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        private const string DefaultSaveFile = "save";
+        private SavingSystem _savingSystem;
+
+        private void Awake()
+        {
+            _savingSystem = GetComponent<SavingSystem>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.S)) _savingSystem.Save(DefaultSaveFile);
+            if (Input.GetKeyDown(KeyCode.L)) _savingSystem.Load(DefaultSaveFile);
+        }
+    }
+}
