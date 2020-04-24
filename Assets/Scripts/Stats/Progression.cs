4d3bb8b0bf1a3651c78bc5880251f903c314d8ad
Salikh Magomedov <sm.saarus@gmail.com>
2020-04-24 19:38:43 +0300

    Progression Scriptable Object

---
+++ Assets/Scripts/Stats/Progression.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace RPG.Stats
+{
+    [CreateAssetMenu(fileName = "Progression", menuName = "Stats/New Progression", order = 0)]
+    public class Progression : ScriptableObject
+    {
+        [SerializeField] private ProgressionCharacterClass[] characterClasses;
+        
+        [System.Serializable]
+        private class ProgressionCharacterClass
+        {
+            [SerializeField] private CharacterClass characterClass;
+            [SerializeField] private float[] health;
+        }
+    }
+}
