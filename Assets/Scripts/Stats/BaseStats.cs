e46993488bd05a8a128d1e022f8f61980867cb78
Salikh Magomedov <sm.saarus@gmail.com>
2020-04-24 19:24:11 +0300

    Progression Design

---
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace RPG.Stats
+{
+    public class BaseStats : MonoBehaviour
+    {
+        [Range(1, 99)]
+        [SerializeField] private int startingLevel = 1;
+        [SerializeField] private CharacterClass characterClass = CharacterClass.Grunt;
+    }
+}
