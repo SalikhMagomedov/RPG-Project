36cbb8153c1c45756e822c0f5a85a4a06bb5944a
Salikh Magomedov <sm.saarus@gmail.com>
2020-04-25 22:19:35 +0300

    Saving Experience Points

--- Assets/Scripts/Resources/Experience.cs
+++ Assets/Scripts/Resources/Experience.cs
@@ -1,14 +1,22 @@
-﻿using UnityEngine;
+﻿using RPG.Saving;
+using UnityEngine;
 
 namespace RPG.Resources
 {
-    public class Experience : MonoBehaviour
+    public class Experience : MonoBehaviour, ISaveable
     {
         [SerializeField] private float experiencePoints;
 
         public void GainExperience(float experience)
         {
             experiencePoints += experience;
+        }
+
+        public object CaptureState() => experiencePoints;
+
+        public void RestoreState(object state)
+        {
+            experiencePoints = (float) state;
         }
     }
 }
