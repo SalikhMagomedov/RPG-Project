7b1eac39be9f9b86fde0815db851ebd1bf0401c4
Salikh Magomedov <sm.saarus@gmail.com>
2020-05-04 20:57:18 +0300

    In Game UI

---
+++ Assets/Scripts/Core/CameraFacing.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace RPG.Core
+{
+    public class CameraFacing : MonoBehaviour
+    {
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        private void LateUpdate()
+        {
+            transform.forward = _camera.transform.forward;
+        }
+    }
+}
