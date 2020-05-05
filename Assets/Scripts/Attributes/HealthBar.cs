6304cf4f6727ed43bbd99f3e0055037bb0354684
Salikh Magomedov <sm.saarus@gmail.com>
2020-05-05 20:30:54 +0300

    Disabling Empty Health Bars

--- Assets/Scripts/Attributes/HealthBar.cs
+++ Assets/Scripts/Attributes/HealthBar.cs
@@ -4,11 +4,26 @@ namespace RPG.Attributes
 {
     public class HealthBar : MonoBehaviour
     {
+        private Canvas _canvas;
+        
         [SerializeField] private Health health;
         [SerializeField] private RectTransform foreground;
-        
+
+        private void Awake()
+        {
+            _canvas = GetComponentInChildren<Canvas>();
+        }
+
         private void Update()
         {
+            if (Mathf.Approximately(health.Fraction, 0) || Mathf.Approximately(health.Fraction, 1))
+            {
+                _canvas.enabled = false;
+                return;
+            }
+
+            _canvas.enabled = true;
+            
             foreground.localScale = new Vector3(health.Fraction, 1f, 1f);
         }
     }
