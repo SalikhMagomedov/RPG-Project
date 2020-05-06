4432165bf38807ad675893baee117cca31782429
Salikh Magomedov <sm.saarus@gmail.com>
2020-05-06 19:37:10 +0300

    Boom Microphone Audio Listener

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace RPG.Combat
 {
     public class Weapon : MonoBehaviour
     {
+        [SerializeField] private UnityEvent onHit;
+        
         public void OnHit()
         {
-            
+            onHit.Invoke();
         }
     }
 }
